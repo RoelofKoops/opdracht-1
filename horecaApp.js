03032341ef7a643d01b50b699027844be594f114
roel0Fzo <99055522@mydavinci.nl>
2019-11-06 13:25:43 +0100

    snacks gemaakt

--- horecaApp.js
+++ horecaApp.js
@@ -10,9 +10,11 @@ var frisb = 0;
 var bierb = 0;
 var wijnb = 0;
 var snackb = 0;
+var achtb = 0;
+var zestienb = 0;
 
-var acht;
-var zestien;
+var acht = 0;
+var zestien = 0;
 
 
 
@@ -39,9 +41,14 @@ while(bestelling != 'stop'){
 			snackb = prompt("Hoeveel bitterballen wilt u bestellen (kies 8 of 16)")
 			snack = snack + parseInt(snackb)
 			if (snackb == "8" ){
+				achtb = prompt("Hoeveel schalen van 8 wilt u bestellen")
+				acht = acht + parseInt(achtb)
+
 	
 			}
 				else if (snackb == "16" ){
+					zestienb = prompt("Hoeveel schalen van 16 wilt u bestellen")
+					zestien = zestien + parseInt(zestienb)
 	
 				}
 					else{
@@ -71,15 +78,13 @@ function bon (fris, bier, wijn){
 		document.write(wijn + " wijn: €" + wijn*2.00 + "<br>")
 	}
 
-	if (snack == "8"){
-		snack = acht
-		document.write("8 bitterballen: €4<br>")
+	if (acht > 0){
+		document.write(acht + " schalen met 8 bitterballen: €" + acht*4.00 + "<br>")
 	}
-	if (snack == "16"){
-		snack = zestien
-		document.write("16 bitterballen: €8<br>")
+	if (zestien > 0){
+		document.write(zestien + " schalen met 16 bitterballen: €" + zestien*8.00 + "<br>")
 	}
 	document.write("_________________________________________________________<br>")
 	document.write("totaal: €")
-	document.write(fris * 1.00 + bier * 1.50 + wijn * 2.00 + snackb*0.5 )
+	document.write(fris * 1.00 + bier * 1.50 + wijn * 2.00 + acht * 4.00 + zestien * 8.00 )
  	}
